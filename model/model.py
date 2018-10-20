1ae3e40fdd33bc3dce4ac538a55d2cf3e40a80aa
Jiaxin_Bai <jbai0318@gmail.com>
2018-10-21 01:08:31 +0800

    add model
    too late in the night, midterm is coming.  Unit test not finished, plan to do it next Monday

--- model/model.py
+++ model/model.py
@@ -8,24 +8,71 @@ def get_batch():
 	max_length = 32
 	input_size = 20
 
-
-
 	# try to generate the sequences that are increasing or decreasing
+	x = np.random.rand(batch_size, max_length, input_size)
+	y = np.mean(x, axis=2) > 0.5
 
-	y = np.zeros((batch_size, max_length))
-	y = np.
-
-
-
+	return x, y
 
 
+class Model:
+	def __init__(self, config):
+		self.batch_size = config.batch_size
+		self.input_size = config.input_size
+		self.hidden_size = config.hidden_size
+
+		self.Xs = tf.placeholder(tf.float32, shape=(batch_size, None, input_size))
+		self.isTraining = tf.placeholder(tf.bool)
+		self.Ys = tf.placeholder(tf.int32, shape=(batch_size, None))
+
+		with tf.variable_scope("model", reuse=tf.AUTO_REUSE):
+
+			# first we use GRU cell because LSTM may overfit the data
+			cell = tf.nn.rnn_cell.GRUCell()
+
+			# 'outputs' is a tensor of shape [batch_size, max_time, cell_state_size]
+
+			# defining initial state
+			initial_state = cell.zero_state(self.batch_size, dtype=tf.float32)
+
+			# 'state' is a tensor of shape [batch_size, cell_state_size]
+			self.rnn_outputs, state = tf.nn.dynamic_rnn(cell, input_data, initial_state=initial_state, dtype=tf.float32)
 
 
+			self.add_fully_connected_layers()
+			self.add_loss()
+			self.add_optimizer()
 
 
+	def add_fully_connected_layers(self):
+		reshaped_rnn_outputs = tf.reshape(self.rnn_outputs, [-1, self.hidden_size])
+		# one layer fully connected layer with relu activation, with shape [batch_size*max_length, hidden_size]
+		hidden_layer = tf.layers.dense(reshaped_rnn_outputs, self.hidden_size/2, tf.nn.relu)
+		# last layer with shape [batch_size*max_length, 2]
+		self.reshaped_scores = tf.layers.dense(reshaped_rnn_outputs, 2)
+
+	def add_prediction(self):
+		pass
+
+	def add_loss(self):
+		self.loss = tf.reduce_mean( tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.Ys, logits=self.reshaped_scores))
+
+	def add_optimizer(self):
+        # Adam is used. 
+        optimizer = tf.train.AdamOptimizer(0.001)
+        self.optimize = optimizer.minimize(self.loss)
 
 
+	def save_model(self):
+		pass
 
-class model:
+class Config:
 	def __init__(self):
+		self.batch_size = 300
 
+
+
+
+
+if __name__ == "__main__":
+	get_batch()
---
+++ model/train.py
@@ -0,0 +1,26 @@
+from model import Model
+from model import get_batch
+from model import Config
+import tensorflow as tf 
+
+class Trainer:
+	def __init__(self, model):
+		self.model = model
+
+
+	def train(self):
+		pass
+
+	def test(self):
+		pass
+
+	def save(self):
+		pass
+
+	def load(self):
+		pass
+
+
+
+
+
