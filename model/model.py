f12b56c8300f993e0557c0574d88cc3ce56f48af
Jiaxin_Bai <jbai0318@gmail.com>
2018-10-20 22:22:23 +0800

    reshape

---
+++ model/model.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+
+
+# this is the mock get_batch function
+def get_batch():
+	batch_size = 128
+	max_length = 32
+	input_size = 20
+
+
+
+	# try to generate the sequences that are increasing or decreasing
+
+	y = np.zeros((batch_size, max_length))
+	y = np.
+
+
+
+
+
+
+
+
+
+
+
+
+class model:
+	def __init__(self):
+
